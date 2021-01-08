049e4f838ea832f57f53229df5f592a024ceb1bf
Pozadr <adpozor@gmail.com>
2021-01-08 13:39:05 +0100

    CRUD

--- src/main/java/pl/pozadr/hellomongodb/app/Main.java
+++ src/main/java/pl/pozadr/hellomongodb/app/Main.java
@@ -8,6 +8,9 @@ import pl.pozadr.hellomongodb.model.Toy;
 import pl.pozadr.hellomongodb.model.ToyType;
 import pl.pozadr.hellomongodb.repository.ToyRepo;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class Main {
     private final ToyRepo toyRepo;
@@ -19,11 +22,36 @@ public class Main {
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
+        toyRepo.deleteAll();
         Toy teddyBear = new Toy("Teddy", ToyType.TEDDY_BEAR);
-        toyRepo.save(teddyBear);
         Toy doll = new Toy("Alice", ToyType.DOLL);
-        toyRepo.save(doll);
         Toy legoBlocks = new Toy("Technics", ToyType.LEGO);
+
+        // CREATE
+        System.out.println("CREATE: teddy, doll, lego");
+        toyRepo.save(teddyBear);
+        toyRepo.save(doll);
         toyRepo.save(legoBlocks);
+
+        // READ
+        System.out.println("READ: all");
+        List<Toy> toyList = toyRepo.findAll();
+        toyList.forEach(System.out::println);
+        //UPDATE
+        System.out.println("UPDATE: lego");
+        Optional<Toy> toyOptional = toyRepo.findById(toyList.get(2).getId());
+        if (toyOptional.isPresent()) {
+            Toy toy = toyOptional.get();
+            toy.setName("Duplo");
+            toyRepo.save(toy);
+        }
+        toyRepo.findAll().forEach(System.out::println);
+
+        //DELETE
+        System.out.println("DELETE: doll");
+        toyRepo.delete(doll);
+        toyRepo.findAll().forEach(System.out::println);
+
+
     }
 }
--- src/main/java/pl/pozadr/hellomongodb/model/Toy.java
+++ src/main/java/pl/pozadr/hellomongodb/model/Toy.java
@@ -41,4 +41,13 @@ public class Toy {
     public void setType(ToyType type) {
         this.type = type;
     }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
